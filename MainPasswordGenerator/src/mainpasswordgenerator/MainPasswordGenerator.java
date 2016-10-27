37b071f2df8ec93f80230ccf2442c36cf8c54fec
HenkeK <henrik.kultala@elev.stockholm.se>
2016-10-27 13:22:51 +0200

    Removed some lines

--- MainPasswordGenerator/src/mainpasswordgenerator/MainPasswordGenerator.java
+++ MainPasswordGenerator/src/mainpasswordgenerator/MainPasswordGenerator.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package mainpasswordgenerator;
 
 /**
@@ -11,11 +7,8 @@ package mainpasswordgenerator;
  */
 public class MainPasswordGenerator {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
     }
     
 }
